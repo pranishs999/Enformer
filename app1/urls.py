6140bfbc2035e32500ce30724d4923f34bb849e5
pranish shrestha <93386115+pranishs999@users.noreply.github.com>
2024-04-15 16:33:04 +0000

    signup start

--- app1/urls.py
+++ app1/urls.py
@@ -5,5 +5,4 @@ from . import views
 
 urlpatterns = [
    path('', views.index,name='blog'),
-   path('', views.index,name='blog'),
 ]
--- app1/views.py
+++ app1/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from django.contrib.auth import authenticate,logout,login
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'signup.html')
 
 
 def logoutUser(request):
